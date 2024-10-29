268eeb05ea5fa2a4b77805b863493e67bffa77ce
BiCuro <87143970+BICuror@users.noreply.github.com>
2024-10-30 00:28:32 +0300

    Add component container

--- Assets/Scripts/Combat/Entities/CombatEntity.cs
+++ Assets/Scripts/Combat/Entities/CombatEntity.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class CombatEntity : MonoBehaviour
+public abstract class CombatEntity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<Type, MonoBehaviour> _conmponentDictionary;
+
+    public void AddComponent<T>(T component) where T : MonoBehaviour
     {
-        
+        _conmponentDictionary.Add(T, component);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveComponent<T>(T component) where T: MonoBehaviour
     {
-        
+        _conmponentDictionary.Remove(T);
     }
 }

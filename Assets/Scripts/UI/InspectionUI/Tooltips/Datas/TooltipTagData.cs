3ac2c7046695d0fe82292c1c3fbd5186d43c846e
BiCuro <poikljiyhgtrfedawqss@yandex.ru>
2026-02-02 21:13:28 +0300

    Add subpanels localization

--- Assets/Scripts/UI/InspectionUI/Tooltips/Datas/TooltipTagData.cs
+++ Assets/Scripts/UI/InspectionUI/Tooltips/Datas/TooltipTagData.cs
@@ -1,18 +1,21 @@
 using Ligofff.CustomSOIcons;
+using CuroLocalization;
 using UnityEngine;
 
 public abstract class TooltipTagData : ScriptableObject
 {
     [Tooltip("Tag without #")] [SerializeField] private string _tag;
-    [Tooltip("Value which will be shown in final text")] [SerializeField] private string _tagText;
     [SerializeField] private Color _textColor;
     [SerializeField] private Sprite _iconSprite;
-    [TextArea] [SerializeField] private string _description;
     [SerializeField] private bool _onlyText;
-    public bool OnlyText => _onlyText;
+
+    [Space] [Header("Localization")] 
+    [SerializeField] private string _localizationKey;
+    
     public string Tag => _tag;
-    public string TagText => _tagText;
     [CustomAssetIcon] public Sprite IconSprite => _iconSprite;
-    public string Description => _description;
+    public bool OnlyText => _onlyText;
     public Color TextColor => _textColor;
+    public string TagText => (_localizationKey + "_header").Localize();
+    public string Description => (_localizationKey + "_description").Localize();
 }
